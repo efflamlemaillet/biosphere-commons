b1667bad1e49a10e2e0c9460c00e17fc43fb9316
bryan brancotte <bryan.brancotte@gmail.com>
2016-11-17 16:20:54 +0100

    Delete echo_owner_email.sh

--- scripts/echo_owner_email.sh
+++
@@ -1,6 +0,0 @@
-username=$(cat /opt/slipstream/client/bin/slipstream.context | grep username | sed 's/username = //g')
-cat /opt/slipstream/client/bin/slipstream.context | grep cookie | sed 's/cookie = //g' > /root/.slipstream-cookie
-ss-user-get $username 1> /root/user.xml
-export OWNER_EMAIL=$(cat /root/user.xml | sed 's/ /\n/g' | grep email= | sed 's/email=//g' | sed 's/"//g')
-echo $OWNER_EMAIL
-rm -f /root/user.xml /root/.slipstream-cookie
