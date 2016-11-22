957e6e317d11d235de564e8d18531f22e46e66eb
bryan brancotte <bryan.brancotte@gmail.com>
2016-11-22 10:40:34 +0100

    Update populate_hosts_with_components_name_and_ips.sh

--- scripts/populate_hosts_with_components_name_and_ips.sh
+++ scripts/populate_hosts_with_components_name_and_ips.sh
@@ -49,8 +49,8 @@ populate_hosts_with_components_name_and_ips_on_vm_remove(){
         echo Processing $INSTANCE_NAME
         vpn_address=$(ss-get $INSTANCE_NAME:$ip_field_name)
         echo "Removing instance of $SLIPSTREAM_SCALING_NODE: $INSTANCE_NAME/$INSTANCE_NAME_SAFE, $vpn_address"
-        sed -i '/$INSTANCE_NAME /d' ./infile
-        sed -i '/$INSTANCE_NAME_SAFE /d' ./infile
+        sed -i '/$INSTANCE_NAME /d' /etc/hosts
+        sed -i '/$INSTANCE_NAME_SAFE /d' /etc/hosts
     done
 }
 
