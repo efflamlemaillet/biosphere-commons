e6b6e17de7387f3c4a550ad409c9dc1df78bf5b5
Sandrine Perrin <sandrine.perrin@france-bioinformatique.fr>
2017-03-17 13:10:02 +0100

    ADD new directory script/plugins/docker, with all plugins on docker. ADD new script to install frontend on manager docker.

---
+++ scripts/plugins/docker/install_gui_docker.sh
@@ -0,0 +1,77 @@
+#! /bin/bash
+
+## Library to install GUI on docker from name
+
+SCRIPT=`basename "$0"`
+NAME_GUI=$1
+CONTAINER_NAME=frontend_$NAME_GUI
+PORT=${PORT:-9000}
+DIR=/ifb
+DIR_DATA=/ifb/$NAME_GUI-data
+DIR_CERTS=/ifb/$NAME_GUI-certs
+LOG=/var/log/$NAME_GUI.log
+
+SLIPSTREAM_CONTEXT=$( which ss-display &> /dev/null; echo $?)
+
+msg_info(){
+    echo -e "$@"
+    if [ $SLIPSTREAM_CONTEXT -eq 0 ] ; then ss-display "$@" ; fi
+}
+
+
+msg_err(){
+    echo -e "ERROR: $@"
+    if [ $SLIPSTREAM_CONTEXT -eq 0 ] ; then ss-abort "ERROR: $@" ; fi
+}
+
+
+################################
+## Install frontend           ##
+################################
+
+
+function portainer()
+{
+    
+    mkdir -p $DIR_DATA $DIR_CERTS
+    docker run -d --name $ONTAINER_NAME \
+            -p $PORT:9000 \
+            -v $DIR_CERTS:/certs \
+            -v $DIR_DATA:/data \
+            -v /var/run/docker.sock:/var/run/docker.sock \
+            --name $CONTAINER_NAME \ 
+            portainer/portainer
+    
+    msg_info "Running Containers $NAME_GUI"
+    docker ps
+}
+
+################################
+## Main function              ##
+################################
+
+function usage()
+{
+    echo -e "Script to install frontend manager Docker"
+    echo -e "$SCRIPT frontend-name-available <options>"
+    echo -e "To change port : export PORT:8080."
+    echo -e "List available"
+    echo -e "\t portainer : install portainer, no yet implemented to access on docker swarm manager"
+
+}
+
+
+# Pas de paramètre
+[[ $# -lt 1 ]] && error
+
+# -o : options courtes
+# -l : options longues
+# options=$(getopt -o h,m,s: -l help -- "$@")
+# set -- $options
+
+case "$1" in
+   portainer) portainer
+        break;;
+   *) usage
+	beak;;
+esac
