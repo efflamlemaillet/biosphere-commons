cd691625e308a9396ba500764b87b240f18aeb43
Jlorenzo <jonathan.lorenzo@france-bioinformatique.fr>
2017-04-10 11:50:45 +0200

    FIX functions

--- scripts/cluster/sge/sge_install.sh
+++ scripts/cluster/sge/sge_install.sh
@@ -1,3 +1,33 @@
+check_if_vpn_or_not()
+{
+    component_vpn_name=${component_vpn_name:-vpn}
+    
+    category=$(ss-get ss:category)
+    if [ "$category" == "Deployment" ]; then
+        check_vpn=$(ss-get ss:groups | grep -c ":$component_vpn_name")
+        if [ "$check_vpn" != "0" ]; then
+            vpn_multiplicity=$(ss-get $component_vpn_name:multiplicity)
+            if [ "$vpn_multiplicity" != "0" ]; then
+                USER_NEW=$(ss-get $component_vpn_name:edugain_username)
+                if [ "$(echo $(ss-get net.services.enable) | grep '"vpn"' | wc -l)" == "1" ]; then
+                    IP_PARAMETER=vpn.address
+                else
+                    IP_PARAMETER=hostname
+                fi
+            else
+                USER_NEW=${USER_NEW:-sge-user}
+                IP_PARAMETER=hostname
+            fi
+        else
+            USER_NEW=${USER_NEW:-sge-user}
+            IP_PARAMETER=hostname
+        fi
+    else
+        USER_NEW=${USER_NEW:-sge-user}
+        IP_PARAMETER=hostname
+    fi
+}
+
 initiate_install_edugain()
 {
     pip install scriptine
@@ -92,36 +122,6 @@ install_edugain()
     
     #hostname -F /etc/hostname
     echo "FederatedEntryPoint overlay deploy done"
-}
-
-check_if_vpn_or_not()
-{
-    component_vpn_name=${component_vpn_name:-vpn}
-    
-    category=$(ss-get ss:category)
-    if [ "$category" == "Deployment" ]; then
-        check_vpn=$(ss-get ss:groups | grep -c ":$component_vpn_name")
-        if [ "$check_vpn" != "0" ]; then
-            vpn_multiplicity=$(ss-get $component_vpn_name:multiplicity)
-            if [ "$vpn_multiplicity" != "0" ]; then
-                USER_NEW=$(ss-get $component_vpn_name:edugain_username)
-                if [ "$(echo $(ss-get net.services.enable) | grep '"vpn"' | wc -l)" == "1" ]; then
-                    IP_PARAMETER=vpn.address
-                else
-                    IP_PARAMETER=hostname
-                fi
-            else
-                USER_NEW=${USER_NEW:-sge-user}
-                IP_PARAMETER=hostname
-            fi
-        else
-            USER_NEW=${USER_NEW:-sge-user}
-            IP_PARAMETER=hostname
-        fi
-    else
-        USER_NEW=${USER_NEW:-sge-user}
-        IP_PARAMETER=hostname
-    fi
 }
 
 initiate_variable_global()
@@ -739,8 +739,6 @@ UNSET_parameters(){
 #####
 # NFS
 #####
-msg_info "Configuring NFS..."
-
 # exporting NFS share from master
 NFS_export_pdisk_add()
 {
