cf7872abfe2a5bedca20b3263677eed1518ce7f0
Jlorenzo <jonathan.lorenzo@france-bioinformatique.fr>
2017-05-15 18:00:26 +0200

    minor FIX for ubuntu 16

--- scripts/toolshed/virtual_desktop.sh
+++ scripts/toolshed/virtual_desktop.sh
@@ -14,10 +14,16 @@ install_x2go(){
         yum -y groupinstall Fonts
         yum -y install xorg-x11-fonts-Type1 xorg-x11-fonts-misc
         
-    elif isubuntu; then
+    elif isubuntu 14; then
         apt-get -y install x2goserver x2goserver-xsession
         apt-get -y install xfce4
         #apt-get -y install kde-plasma-desktop
+    elif isubuntu 16; then
+        apt-get -y install software-properties-common
+        add-apt-repository ppa:x2go/stable -y
+        apt-get update
+        apt-get -y install x2goserver x2goserver-xsession
+        apt-get -y install xfce4
     else
         echo "unsupported os"
         exit
