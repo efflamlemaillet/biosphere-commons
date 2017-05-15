ee874a95df87956fce683f5afcd0fd7d42a166ad
Jlorenzo <jonathan.lorenzo@france-bioinformatique.fr>
2017-05-15 18:21:04 +0200

    minor FIX for centos 6

--- scripts/toolshed/virtual_desktop.sh
+++ scripts/toolshed/virtual_desktop.sh
@@ -9,6 +9,8 @@ install_x2go(){
     elif iscentos 6; then
         wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
         rpm -ivh epel-release-6-8.noarch.rpm
+        yum -y install x2goserver
+        yum -y install x2goserver-xsession
         yum -y groupinstall Xfce
         yum -y groupinstall Fonts
         yum -y install xorg-x11-fonts-Type1 xorg-x11-fonts-misc
