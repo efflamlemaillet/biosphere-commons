965b72450ff101638d43fac0b5134208bdc9a2b9
Jlorenzo <jonathan.lorenzo@france-bioinformatique.fr>
2017-09-08 17:41:24 +0200

    add other user option for edugain

--- scripts/cluster/cluster_install.sh
+++ scripts/cluster/cluster_install.sh
@@ -173,6 +173,14 @@ initiate_install_edugain_ubuntu16()
 
 install_edugain_ubuntu16()
 {
+    EDUGAIN_OTHERS_USERS=$(ss-get edugain.others.users)
+    if [ -z "$EDUGAIN_OTHERS_USERS" -o $( echo "$EDUGAIN_OTHERS_USERS" | grep -c "@") == 0 ]; then
+        msg_info "No new user to add in federation proxy Eudgain ."
+    else 
+        # Set good syntax
+        others=$(echo $EDUGAIN_OTHERS_USERS | sed -e 's/[^ ],/ ,/' -e 's/,[^ ]/, /' )
+    fi
+    
     if [ "$(echo $(ss-get cloudservice) | grep '"exoscale"' | wc -l)" == "1" ]; then
         OPENSTACK_HOSTNAME=$(ss-get hostname)
     elif [ "$(ss-get cloudservice)" == "ifb-genouest-genostack" ]; then
@@ -197,7 +205,7 @@ install_edugain_ubuntu16()
 
     auto_gen_users
     gen_key_for_user $NEW_USER
-    echo "EMAIL = $(echo_owner_email)" > /home/$NEW_USER/.cyclone
+    echo "EMAIL = $(echo_owner_email) , $others" > /home/$NEW_USER/.cyclone
     publish_pubkey
     allow_others
     
