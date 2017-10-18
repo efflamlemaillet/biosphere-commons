ebdfc0a7c932e6d6212fe0c73b7c70ff375714b5
Jlorenzo <jonathan.lorenzo@france-bioinformatique.fr>
2017-10-18 15:41:45 +0200

    activate pipelining mode

--- scripts/cluster/elasticluster.sh
+++ scripts/cluster/elasticluster.sh
@@ -71,9 +71,11 @@ install_ansible(){
     
     ansible-playbook -M $playbook_dir/library -i $playbook_dir/hosts $playbook_dir/roles/ansible.yml
     
-    #ansible_dir="/etc/ansible"
+    ansible_dir="/etc/ansible"    
     #sed -i '/\[defaults\]/a library = /usr/share/ansible:library' $ansible_dir/ansible.cfg
     #sed -i 's|#host_key_checking.*|host_key_checking = False|' $ansible_dir/ansible.cfg
+    sed -i 's|#pipelining.*|pipelining = True|' $ansible_dir/ansible.cfg
+    
     msg_info "Ansible playbook is installed."
 }
 
