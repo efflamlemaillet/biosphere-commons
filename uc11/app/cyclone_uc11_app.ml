a7496c57d2007609f6335aa7e2c1b048611c8db3
Christophe Blanchet <>
2018-06-20 12:21:34 +0200

    Refonte suite au fork en biosphere-commons

--- uc11/app/cyclone_uc11_app.ml
+++
@@ -1,7 +0,0 @@
-let () =
-  let open Cyclone_uc11.Slipstream_api in
-  login
-    { username = Sys.argv.(1) ; password = Sys.argv.(2) }
-    "https://nuv.la/auth/login"
-  |> Lwt_unix.run
-  |> ignore
