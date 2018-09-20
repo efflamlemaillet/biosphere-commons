f60a23f38ac72c9b40025703e74979e237b2103b
Christophe Blanchet <christophe.blanchet@france-bioinformatique.fr>
2018-09-20 14:28:59 +0200

    Fix bugs, add certificate conf

--- scripts/utils/nextcloud-client.sh
+++ scripts/utils/nextcloud-client.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-# for Ubuntu
-apt-get -y install davfs2
+NEXTCLOUD_HOST=10.158.16.80
 
+# Install client for Ubuntu
+sudo apt-get -y install davfs2
+
+# Configure server certificate
+openssl s_client -connect $NEXTCLOUD_HOST:443 -showcerts </dev/null 2>/dev/null | openssl x509 -outform PEM > mycertificate.pem
+sudo cp mycertificate.pem /etc/davfs2/certs/mycertificate.pem
+sudo sed -i 's/# trust_server_cert/trust_server_cert \/etc\/davfs2\/certs\/mycertificate.pem/' /etc/davfs2/davfs2.conf
+
+# Configure user
 sudo adduser $USER davfs2
-
 mkdir ~/nextcloud
 mkdir ~/.davfs2
 sudo cp  /etc/davfs2/secrets ~/.davfs2/secrets
 sudo chown $USER:$USER ~/.davfs2/secrets
 chmod 600 ~/.davfs2/secrets
 
-sudo echo "https://10.158.16.80/remote.php/webdav/ /home/ubuntu/nextcloud davfs user,rw,auto 0 0" >> /etc/fstab
+# Configure mount
+echo "https://$NEXTCLOUD_HOST/remote.php/webdav/ /home/$USER/nextcloud davfs user,rw,auto 0 0" | sudo tee -a /etc/fstab
