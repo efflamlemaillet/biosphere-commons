7cf915b988f92b284213ea0c716e360cf0dc8b6b
Christophe Blanchet <christophe.blanchet@france-bioinformatique.fr>
2018-09-25 11:31:31 +0200

    Update biosphere-nextcloud-client.sh
    Make it exec as standard user and root
    Add local user 'ubuntu'

--- scripts/utils/biosphere-nextcloud-client.sh
+++ scripts/utils/biosphere-nextcloud-client.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 NEXTCLOUD_HOST=10.158.16.80
+LOCUSER='ubuntu'
+LOCUSER_HOME=`eval echo "~$LOCUSER"`
 
 # Install client for Ubuntu
 cat <<EOF | sudo debconf-set-selections
@@ -11,16 +13,17 @@ sudo apt-get -y install davfs2
 
 # Configure server certificate
 openssl s_client -connect $NEXTCLOUD_HOST:443 -showcerts </dev/null 2>/dev/null | openssl x509 -outform PEM > mycertificate.pem
-sudo cp mycertificate.pem /etc/davfs2/certs/mycertificate.pem
+cp mycertificate.pem /etc/davfs2/certs/mycertificate.pem
 sudo sed -i 's/# trust_server_cert/trust_server_cert \/etc\/davfs2\/certs\/mycertificate.pem/' /etc/davfs2/davfs2.conf
 
 # Configure user
-sudo adduser $USER davfs2
-mkdir ~/nextcloud
-mkdir ~/.davfs2
-sudo cp  /etc/davfs2/secrets ~/.davfs2/secrets
-sudo chown $USER:$USER ~/.davfs2/secrets
-chmod 600 ~/.davfs2/secrets
+sudo adduser $LOCUSER davfs2
+mkdir $LOCUSER_HOME/nextcloud
+chown -R $LOCUSER:$LOCUSER $LOCUSER_HOME/nextcloud
+mkdir $LOCUSER_HOME/.davfs2
+sudo cp  /etc/davfs2/secrets $LOCUSER_HOME/.davfs2/secrets
+sudo chown -R $LOCUSER:$LOCUSER $LOCUSER_HOME/.davfs2
+sudo chmod 600 $LOCUSER_HOME/.davfs2/secrets
 
 # Configure mount
-echo "https://$NEXTCLOUD_HOST/remote.php/webdav/ /home/$USER/nextcloud davfs user,rw,auto 0 0" | sudo tee -a /etc/fstab
+echo "https://$NEXTCLOUD_HOST/remote.php/webdav/ $LOCUSER_HOME/nextcloud davfs user,rw,auto 0 0" | sudo tee -a /etc/fstab
