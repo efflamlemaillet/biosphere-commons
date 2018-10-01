1c93effc7a9a2763c93fc59dda08468a052ab2ba
Christophe Blanchet <christophe.blanchet@france-bioinformatique.fr>
2018-10-01 15:07:26 +0200

    Fix sudo rights

--- scripts/utils/biosphere-nextcloud-client.sh
+++ scripts/utils/biosphere-nextcloud-client.sh
@@ -13,7 +13,7 @@ sudo apt-get -y install davfs2
 
 # Configure server certificate
 openssl s_client -connect $NEXTCLOUD_HOST:443 -showcerts </dev/null 2>/dev/null | openssl x509 -outform PEM > mycertificate.pem
-cp mycertificate.pem /etc/davfs2/certs/mycertificate.pem
+sudo cp mycertificate.pem /etc/davfs2/certs/mycertificate.pem
 sudo sed -i 's/# trust_server_cert/trust_server_cert \/etc\/davfs2\/certs\/mycertificate.pem/' /etc/davfs2/davfs2.conf
 
 # Configure user
