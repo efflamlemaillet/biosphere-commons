c0d22d7bd38602732ae0cbb8fe414df670c998b9
Christophe Blanchet <christophe.blanchet@france-bioinformatique.fr>
2018-10-04 15:36:43 +0200

    Create ebame-install-R-jupyter.sh

---
+++ scripts/utils/ebame-install-R-jupyter.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# to run as root
+apt install -y curl  libgit2-dev libssl-dev
+
+# Install R on Xenial
+add-apt-repository "deb https://pbil.univ-lyon1.fr/CRAN/bin/linux/ubuntu xenial-cran35/"
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+
+apt install -y r-base-dev
+
+R -e "install.packages(c('Matrix', 'igraph', 'huge', 'BiocInstaller', 'gtools'))"
+
+R -e "source('http://bioconductor.org/biocLite.R');biocLite('phyloseq')"
+
+R -e "library(devtools);install_github(\"zdk123/SpiecEasi\")"
+
+# Install R in Jupyter
+# https://irkernel.github.io/docs/IRkernel/0.7/
+
+R -e "install.packages(c('repr', 'IRdisplay', 'crayon', 'pbdZMQ', 'devtools'))"
+R -e "library(devtools);install_github('IRkernel/IRkernel')"
+R -e "IRkernel::installspec()"
+
+#if necessary
+#/opt/miniconda/miniconda3-4.2.12/bin/jupyter notebook --allow-root > /var/log/jupyter/jupyter.log &)
