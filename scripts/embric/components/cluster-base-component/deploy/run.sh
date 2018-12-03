12ae0fed1bcd8e7c7fb9dbbce69c797d6ebe292b
efflam lemaillet <elemaill>
2018-12-04 00:37:18 +0100

    creation du deploy pour le composant de base

---
+++ scripts/embric/components/cluster-base-component/deploy/run.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+rsa_with_passphrase_exists (){
+        file=${1:-~/.ssh/id_rsa}
+        rc=0
+        if [[ -f $file ]]
+        then
+                rc=$(expect -c "
+                spawn ssh-keygen -yf $file
+                expect {
+                        -exact \"Enter passphrase:\" {
+                                exit 1
+                        }
+                        eof {
+                                exit 0
+                        }
+                }
+                ")
+        fi
+}
+
+create_ssh_key(){
+        yesv="n"
+        rsa_with_passphrase_exists
+        if [[ $? -eq 1 ]];then
+                yesv="y"
+        fi
+        yes ${yesv}|ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+
+}
+
+_run () {
+
+        create_ssh_key
+	ss-set hosts-entry $(ss-get hostname) $(ss-get nodename)$(ss-get id)
+
+}
+
+
+
+_run()
