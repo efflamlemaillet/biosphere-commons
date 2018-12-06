a0a4209041d85c848af1cb8bd4ce1c680bd89465
efflam lemaillet <elemaill>
2018-12-06 15:46:37 +0100

    modifs des profil env.sh pour ne pas évaluer PATH

--- scripts/embric/components/spark-hadoop/post-install/run.sh
+++ scripts/embric/components/spark-hadoop/post-install/run.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-#install hadoop
-
-#install hadoop binaries
 install_hadoop(){
 
 	HADOOP_V="2.7.7"
 	HADOOP_PKG="hadoop-$HADOOP_V.tar.gz"
 	HADOOP_LOCAL_DIR="/usr/local/hadoop"
-
 
 	wget http://mirrors.standaloneinstaller.com/apache/hadoop/common/hadoop-$HADOOP_V/$HADOOP_PKG
 
@@ -16,11 +12,11 @@ install_hadoop(){
 	sudo tar -xzvf $HADOOP_PKG -C ${HADOOP_LOCAL_DIR}  --strip-components 1
 
 	touch /etc/profile.d/hadoop-env.sh
-	cat >>/etc/profile.d/hadoop-env.s <<EOF
+	cat >>/etc/profile.d/hadoop-env.sh <<EOF
 export HADOOP_V=$HADOOP_V
 export HADOOP_PKG=$HADOOP_PKG
 export HADOOP_LOCAL_DIR=$HADOOP_LOCAL_DIR
-export PATH="'$PATH'":${HADOOP_LOCAL_DIR}/bin
+export PATH=\$PATH:${HADOOP_LOCAL_DIR}/bin
 EOF
 
 }
