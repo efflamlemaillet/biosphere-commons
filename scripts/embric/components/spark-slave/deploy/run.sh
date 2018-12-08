9e8d6797253f0eb0404248ea8e9db85effc52fd2
efflam lemaillet <elemaill>
2018-12-08 12:00:14 +0100

    ajout du deploy pour spark slave

--- scripts/embric/components/spark-slave/deploy/run.sh
+++ scripts/embric/components/spark-slave/deploy/run.sh
@@ -1,16 +1,26 @@
 #!/bin/bash -xe
 
-ss-set component_name $(ss-get nodename)
 
-mkdir -p /root/.ssh/
-touch /root/.ssh/authorized_keys
-master_name=$(ss-get --timeout=3600 master_cpnt_name)
+authorize_masters_ssh(){
+	
+	mkdir -p /root/.ssh/
+	touch /root/.ssh/authorized_keys
+	master_name=$(ss-get --timeout=3600 master_cpnt_name)
 
-master_list=$(ss-get $master_name:ids)
+	master_list=$(ss-get $master_name:ids)
 
-IFS=',' read -ra master_ids <<< "$master_list"
-echo "master info $master_list $master_ids" /var/log/ss-debug.log
-for id in ${master_ids[@]} ; do
-    echo "key of master $id ($master_name) "
-    echo $(ss-get $master_name.$id:public_key) >> /root/.ssh/authorized_keys
-done
+	IFS=',' read -ra master_ids <<< "$master_list"
+	echo "master info $master_list $master_ids" /var/log/ss-debug.log
+	for id in ${master_ids[@]} ; do
+	    echo "key of master $id ($master_name) "
+	    echo $(ss-get $master_name.$id:public_key) >> /root/.ssh/authorized_keys
+	done
+
+}
+
+
+_run(){
+	authorize_masters_ssh
+}
+
+_run
