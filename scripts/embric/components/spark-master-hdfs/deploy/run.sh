d23777b08434d3fe9992fa6b54c82ff8e0e49188
efflam lemaillet <elemaill>
2018-12-09 14:37:00 +0100

    add

--- scripts/embric/components/spark-master-hdfs/deploy/run.sh
+++ scripts/embric/components/spark-master-hdfs/deploy/run.sh
@@ -1,21 +1,48 @@
 #!/bin/bash -xe
 
-#create if not exist and 
-#add slave to hadoop config
-. ~/.bashrc
-touch ${HADOOP_LOCAL_DIR}/etc/hadoop/slaves
 
-slave_name=$(ss-get slave_cpnt_name)
+config_hadoop_slaves(){
 
-slaves_list=$(ss-get $slave_name:ids)
+	#create if not exist and 
+	#add slave to hadoop config
+	. ~/.bashrc
+	touch ${HADOOP_LOCAL_DIR}/etc/hadoop/slaves
 
-IFS=',' read -ra slave_ids <<< "$slaves_list"
-truncate -s0 ${HADOOP_LOCAL_DIR}/etc/hadoop/slaves
-#wait for all slave scripts to set hostname
-for id in ${slave_ids[@]} ; do
-    echo $(ss-get $slave_name.$id:hostname) >> ${HADOOP_LOCAL_DIR}/etc/hadoop/slaves
-done
+	slave_name=$(ss-get slave_cpnt_name)
 
-#format hdfs
-${HADOOP_LOCAL_DIR}/bin/hdfs namenode -format
-${HADOOP_LOCAL_DIR}/sbin/start-dfs.sh
+	slaves_list=$(ss-get $slave_name:ids)
+
+	IFS=',' read -ra slave_ids <<< "$slaves_list"
+	truncate -s0 ${HADOOP_LOCAL_DIR}/etc/hadoop/slaves
+	#wait for all slave scripts to set hostname
+	for id in ${slave_ids[@]} ; do
+	    echo $(ss-get $slave_name.$id:hostname) >> ${HADOOP_LOCAL_DIR}/etc/hadoop/slaves
+	done
+}
+
+#$1 = input $2 output $3 = name  $4 = value
+add_property(){
+	xmlstarlet edit -s '//configuration' -t elem -n "property" \
+		-s '//configuration/property[last()]' -t elem -n "name"  -v $3 \
+		-s '//configuration/property[last()]' -t elem -n "value" -v $4 \
+		$1 > $2
+	
+}
+config_hadoop_xml(){
+	add_property "${HADOOP_LOCAL_DIR}/share/hadoop/common/templates/core-site.xml" "${HADOOP_LOCAL_DIR}/etc/hadoop/core-site.xml" "fs.default.name" "hdfs://master.local:9000"
+	add_property "${HADOOP_LOCAL_DIR}/share/hadoop/hdfs/templates/hdfs-site.xml" "${HADOOP_LOCAL_DIR}/etc/hadoop/hdfs-site.xml" "dfs.replication" "3"
+	add_property "${HADOOP_LOCAL_DIR}/share/hadoop/hdfs/templates/hdfs-site.xml" "${HADOOP_LOCAL_DIR}/etc/hadoop/hdfs-site.xml" "dfs.data.dir" "/srv/hadoop/datanode" 
+	add_property "${HADOOP_LOCAL_DIR}/share/hadoop/hdfs/templates/hdfs-site.xml" "${HADOOP_LOCAL_DIR}/etc/hadoop/hdfs-site.xml" "dfs.name.dir" "/srv/hadoop/namenode"
+}
+
+
+
+_run(){
+	. /etc/profile.d/*hadoop*
+	config_hadoop_slaves
+	#format hdfs
+	${HADOOP_LOCAL_DIR}/bin/hdfs namenode -format
+	${HADOOP_LOCAL_DIR}/sbin/start-dfs.sh
+}
+
+_run
