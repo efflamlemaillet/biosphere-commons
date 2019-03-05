3f0d02e2f53526c60cb337da863d13c828c3b59e
efflam lemaillet <elemaill>
2019-03-05 14:46:23 +0100

    Veuillez saisir le message de validation pour vos modifications. Les lignes

--- scripts/embric/components/mpl/post-install/run.sh
+++ scripts/embric/components/mpl/post-install/run.sh
@@ -2,11 +2,12 @@
 
 set_install_var(){
 	export APP_NAME="$1"
+	export CAP_APP_NAME=${APP_NAME^^}
 	export LOCAL_DIR="/usr/local/$APP_NAME"
 	export DATA_DIR="/var/lib/$APP_NAME"
 	export ENV_FILE="/etc/profile.d/$APP_NAME-env.sh"
 	export SC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-	envsubst '$LOCAL_DIR,$DATA_DIR' < $SC_DIR/../config/app-env.sh > $ENV_FILE
+	envsubst '$CAP_APP_NAME,$LOCAL_DIR,$DATA_DIR' < $SC_DIR/../config/app-env.sh > $ENV_FILE
 }
 
 _install(){
