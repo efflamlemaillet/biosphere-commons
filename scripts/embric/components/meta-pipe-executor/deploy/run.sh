c10770ef67c50c3191b423851839ca442d665350
efflam lemaillet <elemaill>
2019-04-10 11:39:25 +0200

    fix nom variable avec un tiret

--- scripts/embric/components/meta-pipe-executor/deploy/run.sh
+++ scripts/embric/components/meta-pipe-executor/deploy/run.sh
@@ -6,14 +6,14 @@ _run(){
 	GIT_DIR=$(basename $METAPIPE_GIT)	
 	#. /etc/profile.d/$COMPONENT_NAME-env.sh
 	touch /etc/profile.d/$COMPONENT_NAME-env.sh
-	export MASTER-HOSTNAME=$(hostname)
+	export MASTER_HOSTNAME=$(hostname)
 	export METAPIPE_HOME='$(ss-get mount_point_path)/metapipe'
 	mkdir -p METAPIPE_HOME
 	mkdir $METAPIPE_HOME
 	echo "export METAPIPE_HOME='/var/lib/metapipe'" >> /etc/profile.d/$COMPONENT_NAME-env.sh
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 	git clone $METAPIPE_GIT
-	envsubst '$MASTER-HOSTNAME' ${DIR}/../config/exec_conf.json.template > ./newpan-tools/exec_conf.json
+	envsubst '$MASTER_HOSTNAME' ${DIR}/../config/exec_conf.json.template > ./newpan-tools/exec_conf.json
 	cd $GIT_DIR
 	./example/services/minio/minio.sh start -c admin password
 	./example/services/authService/auth.sh start
