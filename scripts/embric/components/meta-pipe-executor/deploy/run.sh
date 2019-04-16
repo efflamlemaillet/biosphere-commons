1c379d7f70b59e91760735ebbd46d26f9b42f108
efflam lemaillet <elemaill>
2019-04-16 16:18:52 +0200

    docker error debug

--- scripts/embric/components/meta-pipe-executor/deploy/run.sh
+++ scripts/embric/components/meta-pipe-executor/deploy/run.sh
@@ -3,7 +3,8 @@
 
 _run(){
 	METAPIPE_GIT="https://gitlab.com/uit-sfb/newpan-tools.git"
-	GIT_DIR=$(basename $METAPIPE_GIT .git)	
+	GIT_DIR=$(basename $METAPIPE_GIT .git)
+	
 	#. /etc/profile.d/$COMPONENT_NAME-env.sh
 	touch /etc/profile.d/$COMPONENT_NAME-env.sh
 	export MASTER_HOSTNAME=$(hostname)
@@ -14,6 +15,7 @@ _run(){
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 	git clone $METAPIPE_GIT
 	envsubst '$MASTER_HOSTNAME' < ${DIR}/../config/exec_conf.json.template > ./newpan-tools/exec_conf.json
+	systemctl restart docker
 	cd $GIT_DIR
 	./example/services/minio/minio.sh start -c admin password
 	./example/services/authService/auth.sh start
